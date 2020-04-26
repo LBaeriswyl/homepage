3649e86e73af5b283f9b3df1fed777e78d08aaec
Laurent Baeriswyl <laurentbaeriswyl@protonmail.com>
2020-04-26 19:12:23 +0100

    Connect to mongodb from the server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,29 @@
 // server.js
 const express = require("express");
 const app = express();
+const User = require("./src/User.model");
+const connectDb = require("./src/connection");
 
 const PORT = 8080;
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+app.get("/user-create", async (req, res) => {
+  const user = new User({ username: "userTest" });
+
+  await user.save().then(() => console.log("User created"));
+
+  res.send("User created \n");
+});
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT}`);
+
+  connectDb().then(() => {
+    console.log("MongoDb connected");
+  });
+});
